d5fa31d87c37235f4bed45cad9511e877fc075f7
mataushita <matsushita@d-circle.com>
2021-07-09 08:05:56 +0900

    sec.80

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { products } from 'src/app/products';
 
 @Component({
   selector: 'app-product-list',
@@ -7,27 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  products: any = [
-    {
-      image: "./assets/img/placeholder-image-800x600.png",
-      description: "1 Some quick example text to build on the card title and make up the bulk of the card's content."
-    },
-    {
-      image: "./assets/img/placeholder-image-800x600.png",
-      description: "2 Some quick example text to build on the card title and make up the bulk of the card's content."
-    },
-    {
-      image: "./assets/img/placeholder-image-800x600.png",
-      description: "3 Some quick example text to build on the card title and make up the bulk of the card's content."
-    },
-    {
-      image: "./assets/img/placeholder-image-800x600.png",
-      description: "4 Some quick example text to build on the card title and make up the bulk of the card's content."
-    },
-  ]
+  products: any;
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.products = products;
   }
 
 }
