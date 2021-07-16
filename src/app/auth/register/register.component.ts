10efe246daf8b8c391fe2d4db72195e2df2b6759
mataushita <matsushita@d-circle.com>
2021-07-16 10:45:08 +0900

    フロントエンドとバックエンドを結合

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
     selector: 'app-register',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-    test : Date = new Date();
-    focus: any;
-    focus1: any;
-    constructor() { }
+    errors: any = [];
+
+    constructor(
+        private authService: AuthService,
+        private router: Router
+    ) { }
 
     ngOnInit() {}
+
+    register(registerForm: NgForm) {
+        this.authService.register(registerForm.value).subscribe(
+          (result) => {
+              console.log('success');
+              this.router.navigate(['/login']);
+          },
+          (err: HttpErrorResponse) => {
+              this.errors = err.error.errors;
+              console.error(err)}
+        )
+    }
 }
